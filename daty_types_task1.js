b88adf4e4be56333747ac229367effec79814141
OskarH13 <161576666+OskarH13@users.noreply.github.com>
2024-03-15 13:02:02 +0100

    Änderung Java

---
+++ daty_types_task1.js
@@ -0,0 +1,15 @@
+// String (Zeichenkette)
+let name = "Oskar Herzog";
+console.log(name); // Überprüfen wir den Wert von 'name'
+
+// Number (Zahl)
+let alter = 25;
+console.log(alter); // Überprüfen wir den Wert von 'alter'
+
+// Boolean (Boolescher Wert)
+let magProgrammieren = true;
+console.log(magProgrammieren); // Überprüfen wir den Wert von 'magProgrammieren'
+
+// null
+let keineVariable = null;
+console.log(keineVariable); // Überprüfen wir den Wert von 'keineVariable
---
+++ daty_types_task2.js
@@ -0,0 +1,14 @@
+// String (Zeichenkette)
+let name = "Oskar Herzog";
+console.log("Name:", name);
+
+// Number (Zahl)
+let alter = 44;
+console.log("Alter:", alter);
+
+// Boolean (Boolescher Wert)
+let magProgrammieren = true;
+console.log("Mag Programmieren:", magProgrammieren);
+
+
+
