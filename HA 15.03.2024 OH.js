7f62055a8d23eb5ccd26d465082b9dc16621c6ee
OskarH13 <161576666+OskarH13@users.noreply.github.com>
2024-03-15 15:00:47 +0100

    HA OH 15.03.2024

---
+++ HA 15.03.2024 OH.js
@@ -0,0 +1,101 @@
+let lieblingsBuch = "Der Herr der Ringe";
+
+console.log("Der Wert der Variable lieblingsBuch ist " + lieblingsBuch);
+console.log("Variable lieblingsBuch hat den Typ " + typeof lieblingsBuch);
+
+let anzahlGeschwister = 0;
+
+console.log("Der Wert der Variable anzahlGeschwister ist " + anzahlGeschwister);
+console.log("Variable anzahlGeschwister hat den Typ " + typeof anzahlGeschwister);
+
+let hatFuehrerschein = true;
+
+console.log("Der Wert der Variable hatFuehrerschein ist " + hatFuehrerschein);
+console.log("Variable hatFuehrerschein hat den Typ " + typeof hatFuehrerschein);
+
+let momentaneStimmung = "gut";
+
+console.log("Der Wert der Variable momentaneStimmung ist " + momentaneStimmung);
+console.log("Variable momentaneStimmung hat den Typ " + typeof momentaneStimmung);
+
+let naechsteReiseziel;
+
+console.log("Der Wert der Variable naechsteReiseziel ist " + naechsteReiseziel);
+console.log("Variable naechsteReiseziel hat den Typ " + typeof naechsteReiseziel);
+
+let lieblingsFarbe = "Gelb";
+
+console.log("Der Wert der Variable lieblingsFarbe ist " + lieblingsFarbe);
+console.log("Variable lieblingsFarbe hat den Typ " + typeof lieblingsFarbe);
+
+let wohnflaeche = 50;
+
+console.log("Der Wert der Variable wohnflaeche ist " + wohnflaeche);
+console.log("Variable wohnflaeche hat den Typ " + typeof wohnflaeche);
+
+let magKaffee = true;
+
+console.log("Der Wert der Variable magKaffee ist " + magKaffee);
+console.log("Variable magKaffee hat den Typ " + typeof magKaffee);
+
+let aktuellesProjekt = null;
+
+console.log("Der Wert der Variable aktuellesProjekt ist " + aktuellesProjekt);
+console.log("Variable aktuellesProjekt hat den Typ " + typeof aktuellesProjekt);
+
+let letztesAbendessen = "Putengeschnetzeltes"
+
+console.log("Der Wert der Variable letztesAbendessen ist " + letztesAbendessen);
+console.log("Variable letztesAbendessen hat den Typ " + typeof letztesAbendessen);
+
+let geburtstadt = "Eine in Asien";
+
+console.log("Der Wert der Variable geburtstadt ist " + geburtstadt);
+console.log("Variable geburtstadt hat den Typ " + typeof geburtstadt);
+
+let kaffeetassenProTag = 0;
+
+console.log("Der Wert der Variable kaffeetassenProTag ist " + kaffeetassenProTag);
+console.log("Variable kaffeetassenProTag hat den Typ " + typeof kaffeetassenProTag);
+
+let istVegetarier = false;
+
+console.log("Der Wert der Variable istVegetarier ist " + istVegetarier);
+console.log("Variable istVegetarier hat den Typ " + typeof istVegetarier);
+
+let zukunftsWunsch = null;
+
+console.log("Der Wert der Variable zukunftsWunsch ist " + zukunftsWunsch);
+console.log("Variable zukunftsWunsch hat den Typ " + typeof zukunftsWunsch);
+
+let lieblingsSportart = "Schwimmmen"
+
+console.log("Der Wert der Variable lieblingsSportart ist " + lieblingsSportart);
+console.log("Variable lieblingsSportart hat den Typ " + typeof lieblingsSportart);
+
+let lieblingsFilm = "Das Schweigen der Lämmer";
+
+console.log("Der Wert der Variable lieblingsFilm ist " + lieblingsFilm);
+console.log("Variable lieblingsFilm hat den Typ " + typeof lieblingsFilm);
+
+let hoeheInCm = 174;
+
+console.log("Der Wert der Variable hoeheInCm ist " + hoeheInCm);
+console.log("Variable hoeheInCm hat den Typ " + typeof hoeheInCm);
+
+let hatHaustier = false;
+
+console.log("Der Wert der Variable hatHaustier ist " + hatHaustier);
+console.log("Variable hatHaustier hat den Typ " + typeof hatHaustier);
+
+let mittagessenIdee = null;
+
+console.log("Der Wert der Variable mittagessenIdee ist " + mittagessenIdee);
+console.log("Variable mittagessenIdee hat den Typ " + typeof mittagessenIdee);
+
+let lieblingsMusikband;
+
+console.log("Der Wert der Variable lieblingsMusikband ist " + lieblingsMusikband);
+console.log("Variable lieblingsMusikband hat den Typ " + typeof lieblingsMusikband);
+
+
